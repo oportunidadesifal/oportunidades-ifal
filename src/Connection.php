25e79178d02e11c691ac92e1f052386f030fc55f
augustoifal <prof.augusto.ifal@gmail.com>
2020-02-28 16:53:19 -0300

    add configuração do apache e bd

--- src/Connection.php
+++ src/Connection.php
@@ -11,6 +11,7 @@ class Connection extends PDO
         try {
             //localhost
             //$connect = new PDO("mysql:host=127.0.0.1;port=3306;dbname=oportunista", "root", "");
+            
             //heroku
             $connect = new PDO("mysql:host=us-cdbr-iron-east-04.cleardb.net;port=3306;dbname=oportunista", "b45cc372dbb5ce", "943fe5a3");
             return $connect;
